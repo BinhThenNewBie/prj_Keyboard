8d07fce684f51b6a169afdc615d91827992a63d5
BinhLearnToCode <binhdath06811@gmail.com>
2025-11-07 20:56:46 +0700

    tao file

---
+++ Keyboard/Keyboard/src/main/java/com/example/Keyboard/KeyboardwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Keyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyboardwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyboardwebsiteApplication.class, args);
+	}
+
+}
---
+++ Keyboard/Keyboard/src/test/java/com/example/Keyboard/KeyboardwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Keyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyboardwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
